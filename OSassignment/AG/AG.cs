c380de0dfc9463bce30d6b5a9656443a17018136
OVGB <lan12131999@gmail.com>
2019-12-13 17:10:47 +0200

    FIXED AG

--- OSassignment/AG/AG.cs
+++ OSassignment/AG/AG.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 
 namespace OSassignment
-{ 
+{
     public class AG
     {
         List<Process> processes;
         List<Process> readyqueue;
+        List<Process> tmpList;
+        List<Process> mylist;
         int listiterator = 0;
         int quantam = 4;
         int timecount = 0;
@@ -16,29 +18,37 @@ namespace OSassignment
             processes = pList;
             readyqueue = new List<Process>();
             foreach (Process p in processes)
-            { 
+            {
                 p.AGquantam = quantam;
                 totaltime += p.pBurstTime;
             }
             processes.Sort((x, y) => x.pArrivalTime.CompareTo(y.pArrivalTime));
+            mylist = new List<Process>();
+            tmpList = new List<Process>(processes);
         }
 
         public void Simulate()
         {
-            for(int AGTime = 0; AGTime < totaltime; AGTime++)
+
+            for (int AGTime = 0; AGTime < totaltime; AGTime++)
             {
                 updateReadyQueue();
-                if(readyqueue.Count != 0)
+                if (readyqueue.Count != 0)
                 {
                     Process current = readyqueue[0];
                     readyqueue.Remove(current);
-                    for(int i = 1; i <= current.AGquantam; i++,timecount++)
+                    mylist.Add(current);
+                    double nonP = Math.Ceiling(((double)(current.AGquantam) / 2.0));
+                    for (int i = 1; i <= current.AGquantam; i++, timecount++)
                     {
                         updateReadyQueue();
-                        current.pRemTime--;
-                        foreach (Process p in readyqueue)
+                        if (i <= nonP)
                         {
-                            p.pWaitingTime++;
+                            current.pRemTime--;
+                            foreach (Process p in readyqueue)
+                            {
+                                p.pWaitingTime++;
+                            }
                         }
                         if (current.Finished())
                             break;
@@ -47,23 +57,37 @@ namespace OSassignment
                             double total = 0.0;
                             foreach (Process p in readyqueue)
                                 total += p.AGquantam;
-                            current.AGquantam += (int)Math.Ceiling(total / (double)readyqueue.Count);
+                            if (readyqueue.Count != 0)
+                                current.AGquantam += (int)Math.Ceiling((total / (double)readyqueue.Count) * 0.1);
+                            current.pRemTime--;
+                            foreach (Process p in readyqueue)
+                            {
+                                p.pWaitingTime++;
+                            }
+                            if (current.pRemTime > 0) readyqueue.Add(current);
+                            break;
                         }
-                        if(i > Math.Ceiling(((double)(current.AGquantam)/2.0)))
+                        if (i > nonP)
                         {
                             Process temp = current;
                             if (readyqueue.Count != 0)
-                               temp = getMinAGfactor();
-                            if(temp.AGfactor < current.AGfactor)
+                                temp = getMinAGfactor();
+                            if (temp.AGfactor < current.AGfactor)
                             {
-                                current.AGfactor += (current.AGfactor - i);
+
+                                current.AGquantam += (current.AGquantam - i);
                                 readyqueue.Add(current);
                                 readyqueue.Remove(temp);
                                 readyqueue.Insert(0, temp);
                                 break;
                             }
-                        }
+                            current.pRemTime--;
+                            foreach (Process p in readyqueue)
+                            {
+                                p.pWaitingTime++;
+                            }
 
+                        }
                     }
 
                 }
@@ -73,33 +97,31 @@ namespace OSassignment
 
         private void updateReadyQueue()
         {
-            
-            if (true)
+
+            for (int i = 0; i < tmpList.Count; ++i)
             {
-                while (listiterator < processes.Count)
+                if (timecount == tmpList[i].pArrivalTime)
                 {
-                    if (timecount == processes[listiterator].pArrivalTime)
-                    {
-                        readyqueue.Add(processes[listiterator]);
-                    }
-                    else break;
-                    listiterator++;
+                    Process t = tmpList[i];
+                    readyqueue.Add(tmpList[i]);
+                    tmpList.Remove(t);
                 }
+
             }
 
         }
 
         private Process getMinAGfactor()
         {
-                int min = 1000000000;
-                int idx = 0;
-                for(int i = 0; i < readyqueue.Count; i++)
-                    if (readyqueue[i].AGfactor < min)
-                    { 
-                        min = readyqueue[i].AGfactor;
-                        idx = i;
-                    }
-                return readyqueue[idx];
+            int min = 1000000000;
+            int idx = 0;
+            for (int i = 0; i < readyqueue.Count; i++)
+                if (readyqueue[i].AGfactor < min)
+                {
+                    min = readyqueue[i].AGfactor;
+                    idx = i;
+                }
+            return readyqueue[idx];
         }
 
         public System.Tuple<float, float> EvaluateAvgTime()
@@ -124,10 +146,11 @@ namespace OSassignment
             Tuple<float, float> tuple = EvaluateAvgTime();
             Console.WriteLine("Average Wait Time: " + tuple.Item1);
             Console.WriteLine("Average TurnAround Time: " + tuple.Item2);
-            foreach(Process p in processes)
+            foreach (Process p in mylist)
             {
-                Console.WriteLine(p.pWaitingTime);
-                Console.WriteLine(p.pTurnAroundTime);
+                Console.WriteLine(p.pId + 1);
+                //Console.WriteLine (p.pWaitingTime);
+                //Console.WriteLine(p.pTurnAroundTime);
             }
         }
     }
